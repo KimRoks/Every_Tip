ab03ddb5b7b6c8220991103d06516d654ca95ec5
Sonmoham <dnddl505@gmail.com>
2024-01-20 22:43:59 +0900

    Feat: app delegate - di 어셈블러 추가

--- EveryTip/Targets/EveryTip/Sources/AppDelegate.swift
+++ EveryTip/Targets/EveryTip/Sources/AppDelegate.swift
@@ -1,22 +1,34 @@
 import UIKit
+
 import EveryTipPresentation
+import Swinject
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    private let assembler = Assembler(container: .shared)
 
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
     ) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        
+        assemble()
+        
         let viewController = UIViewController()
         viewController.view.backgroundColor = .white
         window?.rootViewController = viewController
-        window?.makeKeyAndVisible()
+        
         AppIOSTestUI.hello()
         return true
     }
 
+    private func assemble() {
+        assembler.apply(assemblies: [
+            // TODO: 레이어 assembly 등 등록
+        ])
+    }
 }
