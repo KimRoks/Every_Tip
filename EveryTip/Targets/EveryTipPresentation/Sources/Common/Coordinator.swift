acb1fbbe5f696693a16d6da7bbdbb88cbdb6e7a3
Sonmoham <dnddl505@gmail.com>
2024-01-20 23:07:07 +0900

    Feat: Coordinator 프로토콜 구현, 기본 구현 추가

---
+++ EveryTip/Targets/EveryTipPresentation/Sources/Common/Coordinator.swift
@@ -0,0 +1,34 @@
+//
+//  Coordinator.swift
+//  EveryTip
+//
+//  Created by 손대홍 on 1/20/24.
+//  Copyright © 2024 EveryTip. All rights reserved.
+//
+
+import UIKit
+
+public protocol Coordinator: AnyObject {
+    var parentCoordinator: Coordinator? { get set }
+    var childCoordinators: [Coordinator] { get set }
+    var navigationController: UINavigationController { get set }
+    
+    func start()
+    func didFinish()
+}
+
+public extension Coordinator {
+    func append(child: Coordinator) {
+        childCoordinators.append(child)
+    }
+    
+    /**
+     didFinish 시
+     */
+    func remove(child: Coordinator) {
+        guard let targetIndex = childCoordinators.firstIndex(where: { $0 === child }) else {
+            return
+        }
+        childCoordinators.remove(at: targetIndex)
+    }
+}
