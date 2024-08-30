6d5e0c2534c31ea64b02dc881a6397d454fe02ec
KimRoks <113083860+KimRoks@users.noreply.github.com>
2024-08-30 18:30:24 +0900

    refactor: 카테고리를 enum으로 관리하도록 변경

--- EveryTip/Targets/EveryTipPresentation/Sources/Common/Constants.swift
+++ EveryTip/Targets/EveryTipPresentation/Sources/Common/Constants.swift
@@ -21,110 +21,79 @@ public struct Constants {
         ]
     }
     
-    public struct Category {
-        public struct categoryCellItems {
-            public let image: UIImage?
-            public let title: String
-            public let color: UIColor
+    public enum Category: CaseIterable {
+        case hobby
+        case it
+        case health
+        case finance
+        case entertainment
+        case sports
+        case games
+        case social
+        
+        var image: UIImage? {
+            switch self {
+            case .hobby:
+                return UIImage(systemName: "house.fill")
+            case .it:
+                return UIImage(systemName: "globe")
+            case .health:
+                return UIImage(systemName: "heart.fill")
+            case .finance:
+                return UIImage(systemName: "creditcard.fill")
+            case .entertainment:
+                return UIImage(systemName: "play.square.fill")
+            case .sports:
+                return UIImage(systemName: "dumbbell.fill")
+            case .games:
+                return UIImage(systemName: "gamecontroller.fill")
+            case .social:
+                return UIImage(systemName: "antenna.radiowaves.left.and.right")
+            }
         }
         
-        public static let tableViewItems: [categoryCellItems] = [
-            hobby,
-            it,
-            health,
-            finance,
-            entertainment,
-            sports,
-            games,
-            social
-        ]
+        var title: String {
+            switch self {
+            case .hobby:
+                return "취미/생활"
+            case .it:
+                return "IT"
+            case .health:
+                return "건강"
+            case .finance:
+                return "금융"
+            case .entertainment:
+                return "연예/방송"
+            case .sports:
+                return "스포츠"
+            case .games:
+                return "게임"
+            case .social:
+                return "사회"
+            }
+        }
         
-        private static let hobby = categoryCellItems(
-            image: UIImage(systemName: "house.fill"),
-            title: "취미/생활",
-            color: UIColor(
-                red: 0.59,
-                green: 0.74,
-                blue: 0.35,
-                alpha: 1.00
-            )
-        )
+        var color: UIColor {
+            switch self {
+            case .hobby:
+                return UIColor(red: 0.59, green: 0.74, blue: 0.35, alpha: 1.00)
+            case .it:
+                return UIColor(red: 0.31, green: 0.51, blue: 0.90, alpha: 1.00)
+            case .health:
+                return UIColor(red: 0.87, green: 0.39, blue: 0.36, alpha: 1.00)
+            case .finance:
+                return UIColor(red: 0.96, green: 0.79, blue: 0.52, alpha: 1.00)
+            case .entertainment:
+                return UIColor(red: 0.88, green: 0.53, blue: 0.66, alpha: 1.00)
+            case .sports:
+                return UIColor(red: 0.34, green: 0.40, blue: 0.53, alpha: 1.00)
+            case .games:
+                return UIColor(red: 0.67, green: 0.67, blue: 0.67, alpha: 1.00)
+            case .social:
+                return UIColor(red: 0.58, green: 0.54, blue: 0.69, alpha: 1.00)
+            }
+        }
         
-        private static let it = categoryCellItems(
-            image: UIImage(systemName: "globe"),
-            title: "IT",
-            color: UIColor(
-                red: 0.31,
-                green: 0.51,
-                blue: 0.90,
-                alpha: 1.00
-            )
-        )
-        
-        private static let health = categoryCellItems(
-            image: UIImage(systemName: "heart.fill"),
-            title: "건강",
-            color: UIColor(
-                red: 0.87,
-                green: 0.39,
-                blue: 0.36,
-                alpha: 1.00
-            )
-        )
-        
-        private static let finance = categoryCellItems(
-            image: UIImage(systemName: "creditcard.fill"),
-            title: "금융",
-            color: UIColor(
-                red: 0.96,
-                green: 0.79,
-                blue: 0.52,
-                alpha: 1.00
-            )
-        )
-        
-        private static let entertainment = categoryCellItems(
-            image: UIImage(systemName: "play.square.fill"),
-            title: "연예/방송",
-            color: UIColor(
-                red: 0.88,
-                green: 0.53,
-                blue: 0.66,
-                alpha: 1.00
-            )
-        )
-        
-        private static let sports = categoryCellItems(
-            image: UIImage(systemName: "dumbbell.fill"),
-            title: "스포츠",
-            color: UIColor(
-                red: 0.34,
-                green: 0.40,
-                blue: 0.53,
-                alpha: 1.00
-            )
-        )
-        
-        private static let games = categoryCellItems(
-            image: UIImage(systemName: "gamecontroller.fill"),
-            title: "게임",
-            color: UIColor(
-                red: 0.67,
-                green: 0.67,
-                blue: 0.67,
-                alpha: 1.00
-            )
-        )
-        
-        private static let social = categoryCellItems(
-            image: UIImage(systemName: "antenna.radiowaves.left.and.right"),
-            title: "사회",
-            color: UIColor(
-                red: 0.58,
-                green: 0.54,
-                blue: 0.69,
-                alpha: 1.00
-            )
-        )
+        public static let tableViewItems: [Category] = Category.allCases
     }
 }
