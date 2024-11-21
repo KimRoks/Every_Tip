b7c485488800ba9128296c079860cc6d95faf183
KimRoks <113083860+KimRoks@users.noreply.github.com>
2024-11-21 14:16:40 +0900

    feat: TokenRepository 구현체 정의 및 의존성 주입

---
+++ EveryTip/Targets/EveryTipData/Sources/Repositories/DefaultTokenRepository.swift
@@ -0,0 +1,39 @@
+//
+//  DefaultTokenRepository.swift
+//  EveryTipData
+//
+//  Created by 김경록 on 10/29/24.
+//  Copyright © 2024 EveryTip. All rights reserved.
+//
+
+import Foundation
+
+import EveryTipDomain
+
+import Alamofire
+import RxSwift
+
+final class DefaultTokenRepository: TokenRepository {
+    func requestToken(email: String, password: String) -> Single<TokenResponse> {
+        return Single.create { single in
+            do {
+                let request = try AuthTarget.requestToken(email: email, password: password).asURLRequest()
+                
+                AF.request(request, interceptor: TokenInterceptor())
+                    .validate(statusCode: 200..<300)
+                    .responseDecodable(of: TokenResponse.self) { response in
+                        switch response.result {
+                        case .success(let token):
+                            single(.success(token))
+                        case .failure(let error):
+                            single(.failure(error))
+                        }
+                    }
+            } catch {
+                single(.failure(error))
+            }
+            
+            return Disposables.create()
+        }
+    }
+}
