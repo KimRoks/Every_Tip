7e6da02f8dea21c95f32593af39248b29be982ad
KimRoks <kimroks3161@gmail.com>
2025-03-05 19:24:06 +0900

    feat: Network Error Case 추가

--- EveryTip/Targets/EveryTipData/Sources/Services/Network/NetworkError.swift
+++ EveryTip/Targets/EveryTipData/Sources/Services/Network/NetworkError.swift
@@ -18,7 +18,8 @@ enum NetworkError: LocalizedError {
     case requestAdaptationError(Error)
     case requestRetryError(Error)
     case baseURLError
-    
+    case invalidEmail
+
     var errorDescription: String? {
         switch self {
         case .httpError(let code):
@@ -39,6 +40,8 @@ enum NetworkError: LocalizedError {
             return "요청 재시도 오류: \(error.localizedDescription)"
         case .baseURLError:
             return "plist에서 BaseURL을 가져오는데에 실패했어요"
+        case .invalidEmail:
+            return "사용 불가능하거나 형식이 잘못된 이메일이에요"
         }
     }
 }
