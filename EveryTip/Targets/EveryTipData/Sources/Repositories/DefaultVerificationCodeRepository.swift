11ae41b0dd6089a487a10b809f029001ddbbbcac
KimRoks <kimroks3161@gmail.com>
2025-03-14 20:44:19 +0900

    feat: VerificationCodeRepo 구현체 정의

---
+++ EveryTip/Targets/EveryTipData/Sources/Repositories/DefaultVerificationCodeRepository.swift
@@ -0,0 +1,71 @@
+//
+//  DefaultVerificationCodeRepository.swift
+//  EveryTipData
+//
+//  Created by 김경록 on 3/14/25.
+//  Copyright © 2025 EveryTip. All rights reserved.
+//
+
+import Foundation
+
+import EveryTipDomain
+
+import Alamofire
+import RxSwift
+
+struct DefaultVerificationCodeRepository: VerificationCodeRepository {
+    let session: Session?
+    
+    init(session: Session? = .default) {
+        self.session = session
+    }
+    
+    func requestCode(with email: String) -> Completable {
+        Completable.create { completable in
+            do {
+                let request = try AuthTarget.postVerificaitonCode(email: email).asURLRequest()
+                let task = self.session?.request(request, interceptor: nil)
+                    .validate(statusCode: 200..<300)
+                    .responseDecodable(of: BaseResponseDTO.self) { response in
+                        switch response.result {
+                        case .success(let result):
+                            // 통신 자체는 성공했으나 이메일 사용 불가등의 경우로 API가 Code를 Fail로 반환하는 경우를 처리
+                            if result.code == "FAIL" {
+                                completable(.error(NetworkError.invalidEmail))
+                            }
+                            completable(.completed)
+                        case .failure(let error):
+                            completable(.error(error))
+                        }
+                    }
+                return Disposables.create { task?.cancel() }
+            } catch {
+                completable(.error(error))
+                return Disposables.create()
+            }
+        }
+    }
+    
+    func checkCode(with code: String) -> Completable {
+        Completable.create { completable in
+            do {
+                let request = try AuthTarget.getCheckVerificationCode(code: code).asURLRequest()
+                let task = self.session?.request(request, interceptor: nil)
+                    .validate(statusCode: 200..<300)
+                    .responseDecodable(of: BaseResponseDTO.self) { response in
+                        switch response.result {
+                        case .success(_):
+                            completable(.completed)
+                        case .failure(let error):
+                            completable(.error(error))
+                        }
+                    }
+                return Disposables.create { task?.cancel() }
+                
+            } catch {
+                completable(.error(error))
+                return Disposables.create()
+            }
+        }
+    }
+}
