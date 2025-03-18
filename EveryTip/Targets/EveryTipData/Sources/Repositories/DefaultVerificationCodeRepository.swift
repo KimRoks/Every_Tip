2af472bab826bb1de0e7d25b2a80ced90690f120
KimRoks <kimroks3161@gmail.com>
2025-03-18 17:14:37 +0900

    refactor: do Catch문 -> guard문으로 수정

--- EveryTip/Targets/EveryTipData/Sources/Repositories/DefaultVerificationCodeRepository.swift
+++ EveryTip/Targets/EveryTipData/Sources/Repositories/DefaultVerificationCodeRepository.swift
@@ -22,50 +22,50 @@ struct DefaultVerificationCodeRepository: VerificationCodeRepository {
     
     func requestCode(with email: String) -> Completable {
         Completable.create { completable in
-            do {
-                let request = try AuthTarget.postVerificaitonCode(email: email).asURLRequest()
-                let task = self.session?.request(request, interceptor: nil)
-                    .validate(statusCode: 200..<300)
-                    .responseDecodable(of: BaseResponseDTO.self) { response in
-                        switch response.result {
-                        case .success(let result):
-                            // 통신 자체는 성공했으나 이메일 사용 불가등의 경우로 API가 Code를 Fail로 반환하는 경우를 처리
-                            if result.code == "FAIL" {
-                                completable(.error(NetworkError.invalidEmail))
-                            }
-                            completable(.completed)
-                        case .failure(let error):
-                            completable(.error(error))
-                        }
-                    }
-                return Disposables.create { task?.cancel() }
-            } catch {
-                completable(.error(error))
+            guard let request = try? AuthTarget.postVerificationEmail(email: email).asURLRequest() else {
+                completable(.error(NetworkError.invalidURLError))
                 return Disposables.create()
             }
+            
+            let task = self.session?.request(request, interceptor: nil)
+                .validate(statusCode: 200..<300)
+                .responseDecodable(of: BaseResponseDTO.self) { response in
+                    switch response.result {
+                    case .success(let result):
+                        // 통신 자체는 성공했으나 이메일 사용 불가등의 경우로 API가 Code를 Fail로 반환하는 경우를 처리
+                        if result.code == "FAIL" {
+                            completable(.error(NetworkError.invalidEmail))
+                        }
+                        completable(.completed)
+                    case .failure(let error):
+                        completable(.error(error))
+                    }
+                }
+            return Disposables.create { task?.cancel() }
+            
         }
     }
     
     func checkCode(with code: String) -> Completable {
         Completable.create { completable in
-            do {
-                let request = try AuthTarget.getCheckVerificationCode(code: code).asURLRequest()
-                let task = self.session?.request(request, interceptor: nil)
-                    .validate(statusCode: 200..<300)
-                    .responseDecodable(of: BaseResponseDTO.self) { response in
-                        switch response.result {
-                        case .success(_):
-                            completable(.completed)
-                        case .failure(let error):
-                            completable(.error(error))
-                        }
-                    }
-                return Disposables.create { task?.cancel() }
-                
-            } catch {
-                completable(.error(error))
+            
+            guard let request = try? AuthTarget.getCheckVerificationCode(code: code).asURLRequest() else {
+                completable(.error(NetworkError.invalidURLError))
                 return Disposables.create()
             }
+            
+            let task = self.session?.request(request, interceptor: nil)
+                .validate(statusCode: 200..<300)
+                .responseDecodable(of: BaseResponseDTO.self) { response in
+                    switch response.result {
+                    case .success(_):
+                        completable(.completed)
+                    case .failure(let error):
+                        completable(.error(error))
+                    }
+                }
+            return Disposables.create { task?.cancel() }
+            
         }
     }
 }
