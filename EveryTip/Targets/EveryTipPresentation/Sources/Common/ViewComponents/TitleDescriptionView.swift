a2abd6569d01070a9d364598c3a055f2c305cea8
KimRoks <kimroks3161@gmail.com>
2025-03-19 18:39:52 +0900

    chore: 이거 겹칠거임

---
+++ EveryTip/Targets/EveryTipPresentation/Sources/Common/ViewComponents/TitleDescriptionView.swift
@@ -0,0 +1,79 @@
+//
+//  TitleDescriptionView.swift
+//  EveryTipPresentation
+//
+//  Created by 김경록 on 2/12/25.
+//  Copyright © 2025 EveryTip. All rights reserved.
+//
+
+import UIKit
+
+import EveryTipDesignSystem
+
+import SnapKit
+
+final class TitleDescriptionView: UIView {
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .et_pretendard(
+            style: .bold,
+            size: 24
+        )
+        label.textColor = .black
+        
+        return label
+    }()
+    
+    private let subTitleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .et_pretendard(
+            style: .regular,
+            size: 16
+        )
+        label.numberOfLines = 0
+        
+        label.textColor = UIColor(hex: "#777777")
+        
+        return label
+    }()
+
+    init(title: String, subTitle: String) {
+        super.init(frame: .zero)
+        setupLayout()
+        setupConstraints()
+        configure(title: title, subTitle: subTitle)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupLayout() {
+        self.addSubViews(
+            titleLabel,
+            subTitleLabel
+        )
+    }
+    
+    private func setupConstraints() {
+        
+        self.snp.makeConstraints {
+            $0.height.equalTo(56)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(self.snp.top)
+            $0.leading.equalTo(self.snp.leading)
+        }
+        
+        subTitleLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(8)
+            $0.leading.equalTo(self.snp.leading)
+        }
+    }
+    
+    private func configure(title: String, subTitle: String) {
+        titleLabel.text = title
+        subTitleLabel.text = subTitle
+    }
+}
