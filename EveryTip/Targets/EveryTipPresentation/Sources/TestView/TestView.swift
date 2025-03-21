27690504d8b05902050fbfc2f287ba4747e0b7ae
KimRoks <kimroks3161@gmail.com>
2025-03-21 16:28:11 +0900

    refactor: 서치관련 뷰 묶음 및 일부 인스턴스 네이밍 수정

--- EveryTip/Targets/EveryTipPresentation/Sources/TestView/TestView.swift
+++ EveryTip/Targets/EveryTipPresentation/Sources/TestView/TestView.swift
@@ -12,8 +12,6 @@ import EveryTipDomain
 
 import SnapKit
 
-// TODO: isHidden 될 뷰 묶고 SortedPostTableView만들기
-
 final class TestView: UIViewController {
     weak var coordinator: TestViewCoordinator?
     
@@ -24,7 +22,7 @@ final class TestView: UIViewController {
         
         return view
     }()
-
+    
     private let previousButton: UIButton = {
         let button = UIButton(type: .system)
         button.setImage(UIImage(systemName: "chevron.backward"), for: .normal)
@@ -33,7 +31,13 @@ final class TestView: UIViewController {
         return button
     }()
     
-    private let textFieldView: UIView = {
+    private let recentSearchView: UIView = {
+        let view = UIView()
+        
+        return view
+    }()
+    
+    private let searchTextFieldView: UIView = {
         let view = UIView()
         view.layer.cornerRadius = 20
         view.layer.masksToBounds = true
@@ -55,17 +59,16 @@ final class TestView: UIViewController {
         button.setImage(.et_getImage(for: .searchIcon), for: .normal)
         button.tintColor = .et_brandColor2
         
-        
         return button
     }()
     
-    private let ddd: UIView = {
+    private let manageRecentSearchView: UIView = {
         let view = UIView()
         
         return view
     }()
     
-    private let recentWordLabel: UILabel = {
+    private let recentSearchLabel: UILabel = {
         let label = UILabel()
         label.text = "최근 검색어"
         label.font = .et_pretendard(style: .medium, size: 16)
@@ -73,7 +76,7 @@ final class TestView: UIViewController {
         return label
     }()
     
-    private let removeButton: UIButton = {
+    private let removeAllButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("전체 삭제", for: .normal)
         button.titleLabel?.font = .et_pretendard(style: .medium, size: 16)
@@ -83,7 +86,7 @@ final class TestView: UIViewController {
         return button
     }()
     
-    private let recentTableView: UITableView = {
+    private let recentSearchTableView: UITableView = {
         let tableView = UITableView()
         
         return tableView
@@ -103,48 +106,51 @@ final class TestView: UIViewController {
         setupconstraints()
         self.navigationController?.isNavigationBarHidden = true
         setupTableView()
-        
+//        changeView()
         
         view.backgroundColor = .white
         print(tip!)
     }
     
     private func setupTableView() {
-        recentTableView.delegate = self
-        recentTableView.dataSource = self
-        recentTableView.register(
+        recentSearchTableView.delegate = self
+        recentSearchTableView.dataSource = self
+        recentSearchTableView.register(
             RecentCell.self,
             forCellReuseIdentifier: RecentCell.reuseIdentifier
         )
         
-        recentTableView.rowHeight = UITableView.automaticDimension
-        recentTableView.separatorStyle = .none
+        recentSearchTableView.rowHeight = UITableView.automaticDimension
+        recentSearchTableView.separatorStyle = .none
     }
- 
+    
     private func setupLayout() {
-        
         searchStyleNavigationView.addSubViews(
             previousButton,
-            textFieldView,
+            searchTextFieldView,
             searchButton
         )
         
-        ddd.addSubViews(
-            recentWordLabel,
-            removeButton
+        manageRecentSearchView.addSubViews(
+            recentSearchLabel,
+            removeAllButton
+        )
+        
+        recentSearchView.addSubViews(
+            manageRecentSearchView,
+            recentSearchTableView
         )
         
         view.addSubViews(
             searchStyleNavigationView,
-            ddd,
-            recentTableView
+            recentSearchView
         )
         
-        textFieldView.addSubview(searchTextField)
+        searchTextFieldView.addSubview(searchTextField)
     }
     
     private func setupconstraints() {
-                
+        
         searchStyleNavigationView.snp.makeConstraints {
             $0.top.equalTo(view.safeAreaLayoutGuide)
             $0.leading.trailing.equalTo(view.safeAreaLayoutGuide)
@@ -156,16 +162,16 @@ final class TestView: UIViewController {
             $0.leading.equalTo(searchStyleNavigationView.snp.leading).offset(20)
         }
         
-        textFieldView.snp.makeConstraints {
+        searchTextFieldView.snp.makeConstraints {
             $0.top.bottom.equalTo(searchStyleNavigationView).inset(10)
             $0.leading.equalTo(searchStyleNavigationView.snp.leading).offset(45)
             $0.trailing.equalTo(searchStyleNavigationView.snp.trailing).offset(-65)
         }
         
         searchTextField.snp.makeConstraints {
-            $0.top.bottom.equalTo(textFieldView)
-            $0.leading.equalTo(textFieldView).offset(20)
-            $0.trailing.equalTo(textFieldView.snp.trailing).offset(-10)
+            $0.top.bottom.equalTo(searchTextFieldView)
+            $0.leading.equalTo(searchTextFieldView).offset(20)
+            $0.trailing.equalTo(searchTextFieldView.snp.trailing).offset(-10)
         }
         
         searchButton.snp.makeConstraints {
@@ -173,26 +179,32 @@ final class TestView: UIViewController {
             $0.trailing.equalTo(searchStyleNavigationView.snp.trailing).offset(-20)
         }
         
-        ddd.snp.makeConstraints {
+        recentSearchView.snp.makeConstraints {
             $0.top.equalTo(searchStyleNavigationView.snp.bottom).offset(20)
             $0.leading.trailing.equalTo(view.safeAreaLayoutGuide)
-            $0.height.equalTo(recentWordLabel.snp.height)
-        }
-        
-        recentWordLabel.snp.makeConstraints {
-            $0.centerY.equalTo(ddd.snp.centerY)
-            $0.leading.equalTo(ddd.snp.leading).offset(20)
-        }
-        
-        removeButton.snp.makeConstraints {
-            $0.centerY.equalTo(ddd.snp.centerY)
-            $0.trailing.equalTo(ddd.snp.trailing).offset(-20)
-        }
-        
-        recentTableView.snp.makeConstraints {
-            $0.top.equalTo(ddd.snp.bottom).offset(24)
-            $0.leading.trailing.equalTo(view.safeAreaLayoutGuide)
             $0.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        manageRecentSearchView.snp.makeConstraints {
+            $0.top.equalTo(recentSearchView.snp.top)
+            $0.leading.trailing.equalTo(recentSearchView)
+            $0.height.equalTo(recentSearchLabel.snp.height)
+        }
+        
+        recentSearchLabel.snp.makeConstraints {
+            $0.centerY.equalTo(manageRecentSearchView.snp.centerY)
+            $0.leading.equalTo(manageRecentSearchView.snp.leading).offset(20)
+        }
+        
+        removeAllButton.snp.makeConstraints {
+            $0.centerY.equalTo(manageRecentSearchView.snp.centerY)
+            $0.trailing.equalTo(manageRecentSearchView.snp.trailing).offset(-20)
+        }
+        
+        recentSearchTableView.snp.makeConstraints {
+            $0.top.equalTo(manageRecentSearchView.snp.bottom).offset(24)
+            $0.leading.trailing.equalTo(recentSearchView)
+            $0.bottom.equalTo(recentSearchView)
         }
     }
 }
