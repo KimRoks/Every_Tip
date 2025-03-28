dd301c2826e4cc063c497409d4cf5d882a698cbb
KimRoks <kimroks3161@gmail.com>
2025-03-28 19:57:16 +0900

    feat: NickName Repository 정의

---
+++ EveryTip/Targets/EveryTipDomain/Sources/Repositories/UserRepository/NickNameRepository.swift
@@ -0,0 +1,15 @@
+//
+//  NickNameRepository.swift
+//  EveryTipDomain
+//
+//  Created by 김경록 on 3/28/25.
+//  Copyright © 2025 EveryTip. All rights reserved.
+//
+
+import Foundation
+
+import RxSwift
+
+public protocol NickNameRepository {
+    func createRandomNickName() -> Single<String>
+}
