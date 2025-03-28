8963f6fa7c5c59743008a1db78de77c55a28e09e
KimRoks <kimroks3161@gmail.com>
2025-03-28 20:04:56 +0900

    feat: UserUseCase 구현체 작성

--- EveryTip/Targets/EveryTipDomain/Sources/UseCases/User/UserUseCase.swift
+++ EveryTip/Targets/EveryTipDomain/Sources/UseCases/User/UserUseCase.swift
@@ -16,3 +16,35 @@ public protocol UserUseCase {
     func fetchMyProfile() -> Single<User>
     func fetchUserProfile(for userID: Int) -> Single<User>
 }
+
+final class DefaultUserUseCase: UserUseCase {
+    private let nickNameRepository: NickNameRepository
+    private let categoryRepository: CategoryRepository
+    private let profileRepository: ProfileRepository
+    
+    init(
+        profileRepository: ProfileRepository,
+        nickNameRepository: NickNameRepository,
+        categoryRepository: CategoryRepository
+    ) {
+        self.profileRepository = profileRepository
+        self.nickNameRepository = nickNameRepository
+        self.categoryRepository = categoryRepository
+    }
+    
+    public func createRamdomNickName() -> Single<String> {
+        nickNameRepository.createRandomNickName()
+    }
+    
+    public func configureTipCategory(categoryIds: [Int]) -> Completable {
+        categoryRepository.setCategory(categoryIds: categoryIds)
+    }
+    
+    public func fetchMyProfile() -> Single<User> {
+        profileRepository.fetchMyProfile()
+    }
+    
+    public func fetchUserProfile(for userID: Int) -> Single<User> {
+        profileRepository.fetchUserProfile(userID: userID)
+    }
+}
