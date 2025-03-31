5f8e7782d2a5a0e1be8153dfead967647ac5acf5
KimRoks <kimroks3161@gmail.com>
2025-03-31 18:04:54 +0900

    refactor: Account의 DTO,Entity 분리

--- EveryTip/Targets/EveryTipData/Sources/Services/DTO/AccountDTO.swift
+++ EveryTip/Targets/EveryTipData/Sources/Services/DTO/AccountDTO.swift
@@ -1,21 +1,23 @@
 //
-//  AccountResponse.swift
-//  EveryTipDomain
+//  AccountDTO.swift
+//  EveryTipData
 //
-//  Created by 김경록 on 10/28/24.
-//  Copyright © 2024 EveryTip. All rights reserved.
+//  Created by 김경록 on 3/31/25.
+//  Copyright © 2025 EveryTip. All rights reserved.
 //
 
 import Foundation
+
+import EveryTipDomain
 
 public struct AccountResponse: Decodable {
     public let statusCode: Int
     public let code: String
     public let message: String
-    public let data: AccountData
+    public let data: AccountDTO
 }
 
-public struct AccountData: Decodable, Identifiable {
+public struct AccountDTO: Decodable {
     public let id: Int
     public let email: String?
     public let nickName: String?
@@ -27,5 +29,17 @@ public struct AccountData: Decodable, Identifiable {
         case nickName = "nick_name"
         case accessToken = "access_token"
         case refreshToken = "refresh_token"
+    }
+}
+
+extension AccountDTO {
+    func toDomain() -> Account {
+        return Account(
+            id: id,
+            email: email,
+            nickName: nickName,
+            accessToken: accessToken,
+            refreshToken: refreshToken
+        )
     }
 }
