025bf7af7283da24971fc08c956e5ce63d60d2f5
KimRoks <kimroks3161@gmail.com>
2025-04-01 15:47:58 +0900

    refactor: MyProfile 요소 public 처리

--- EveryTip/Targets/EveryTipDomain/Sources/Entities/Model/MyProfile.swift
+++ EveryTip/Targets/EveryTipDomain/Sources/Entities/Model/MyProfile.swift
@@ -9,15 +9,15 @@
 import Foundation
 
 public struct MyProfile {
-    let id: Int
-    let status: Int
-    let nickName: String
-    let profileImage: String?
-    let email: String
-    let registeredDate: String
-    let tipCount: Int
-    let savedTipCount: Int
-    let subscriberCount: Int
+    public let id: Int
+    public let status: Int
+    public let nickName: String
+    public let profileImage: String?
+    public let email: String
+    public let registeredDate: String
+    public let tipCount: Int
+    public let savedTipCount: Int
+    public let subscriberCount: Int
     
     public init(
         id: Int,
