e43e991b81c0bf0791bad4580727a1f089ceba21
KimRoks <kimroks3161@gmail.com>
2025-04-17 11:43:36 +0900

    refactor: ToastDisplayable을 BaseVC가 채택하도록 변경

--- EveryTip/Targets/EveryTipPresentation/Sources/Common/Coordinator.swift
+++ EveryTip/Targets/EveryTipPresentation/Sources/Common/Coordinator.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-public protocol Coordinator: AnyObject, ToastDisplayable {
+public protocol Coordinator: AnyObject {
     var parentCoordinator: Coordinator? { get set }
     var childCoordinators: [Coordinator] { get set }
     var navigationController: UINavigationController { get set }
