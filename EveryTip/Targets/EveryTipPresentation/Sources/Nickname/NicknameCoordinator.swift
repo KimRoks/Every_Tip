ea900db5319e696b6294faef60ec06a9a595ce9a
KimRoks <kimroks3161@gmail.com>
2025-04-22 18:22:10 +0900

    feat: Nickname Coordinator 작성

---
+++ EveryTip/Targets/EveryTipPresentation/Sources/Nickname/NicknameCoordinator.swift
@@ -0,0 +1,56 @@
+//
+//  NicknameCoordinator.swift
+//  EveryTipPresentation
+//
+//  Created by 김경록 on 4/20/25.
+//  Copyright © 2025 EveryTip. All rights reserved.
+//
+
+import UIKit
+
+import EveryTipDomain
+
+import Swinject
+
+protocol NicknameCoordinator: Coordinator {
+    func presentAgreementBottomSheet()
+}
+
+final class DefaultNicknameCoordinator: NicknameCoordinator {
+    
+    var parentCoordinator: (any Coordinator)?
+    
+    var childCoordinators: [any Coordinator] = []
+     
+    var navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        guard let userUseCase = Container.shared.resolve(UserUseCase.self) else {
+            fatalError("의존성 주입이 옳바르지 않습니다!")
+        }
+        
+        let nicknameReactor = NicknameReactor(userUseCase: userUseCase)
+        let nicknameViewController = NicknameViewController(reactor: nicknameReactor)
+        nicknameViewController.coordinator = self
+        
+        navigationController.pushViewController(
+            nicknameViewController,
+            animated: true
+        )
+    }
+    
+    func didFinish() {
+        parentCoordinator?.remove(child: self)
+    }
+    
+    func presentAgreementBottomSheet() {
+        let agreementCoordinator = DefaultCheckAgreementCoordinator(navigationContoller: navigationController)
+        agreementCoordinator.parentCoordinator = self
+        self.append(child: agreementCoordinator)
+        agreementCoordinator.start()
+    }
+}
