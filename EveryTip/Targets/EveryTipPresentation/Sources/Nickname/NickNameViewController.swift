bb5ffc44c7da57f305aea2b9fb8c9198c9bcc3fd
KimRoks <kimroks3161@gmail.com>
2025-04-28 17:28:06 +0900

    refactor: NicknameReactor 동작방식 개선
    - InputState를 뷰에서 체크하도록 변경
    
    - 일부 오탈자 수정

--- EveryTip/Targets/EveryTipPresentation/Sources/Nickname/NickNameViewController.swift
+++ EveryTip/Targets/EveryTipPresentation/Sources/Nickname/NickNameViewController.swift
@@ -22,23 +22,28 @@ final class NicknameViewController: BaseViewController {
             title: "닉네임 설정",
             subTitle: "사용할 닉네임을 설정해주세요.\n고민되면 랜덤 생성하기를 이용하세요!"
         )
-        
         return view
     }()
     
     private let nicknameGuideLineLabel: UILabel = {
         let label = UILabel()
         label.text = "*공백 및 특수문자 불가/한글 최대 8글자/영어 12글자"
-        label.font = .et_pretendard(style: .regular, size: 14)
+        label.font = .et_pretendard(
+            style: .regular,
+            size: 14
+        )
         label.textColor = .et_textColorBlack30
         
         return label
     }()
     
-    private let nicknameLable: UILabel = {
+    private let nicknameLabel: UILabel = {
         let label = UILabel()
         label.text = "닉네임"
-        label.font = .et_pretendard(style: .medium, size: 16)
+        label.font = .et_pretendard(
+            style: .medium,
+            size: 16
+        )
         label.textColor = .et_textColorBlack70
         
         return label
@@ -50,7 +55,6 @@ final class NicknameViewController: BaseViewController {
             hasSecureTextButton: false,
             textFieldRightInset: 40
         )
-        
         textFieldView.textField.placeholder = "닉네임을 설정해주세요"
         
         return textFieldView
@@ -74,20 +78,14 @@ final class NicknameViewController: BaseViewController {
         var configuration = UIButton.Configuration.plain()
         configuration.imagePadding = 6
         configuration.image = UIImage.et_getImage(for: .refresh)
-        
         let attributedTitle = AttributedString(
             "랜덤 설정하기",
             attributes: AttributeContainer([
-                .font: UIFont.et_pretendard(
-                    style: .bold,
-                    size: 14
-                ),
-                .foregroundColor: UIColor.et_textColorBlack30,
+                .font: UIFont.et_pretendard(style: .bold, size: 14),
+                .foregroundColor: UIColor.et_textColorBlack30
             ])
         )
-        
         configuration.attributedTitle = attributedTitle
-        
         let button = UIButton(configuration: configuration)
         button.layer.cornerRadius = 6
         button.backgroundColor = .et_lineGray20
@@ -97,7 +95,10 @@ final class NicknameViewController: BaseViewController {
     
     private let confirmButton: UIButton = {
         let button = UIButton(type: .system)
-        button.setTitle("다음", for: .normal)
+        button.setTitle(
+            "다음",
+            for: .normal
+        )
         button.tintColor = .white
         button.titleLabel?.font = .et_pretendard(
             style: .regular,
@@ -133,7 +134,7 @@ final class NicknameViewController: BaseViewController {
         view.addSubViews(
             titleView,
             nicknameGuideLineLabel,
-            nicknameLable,
+            nicknameLabel,
             nickNameTextField,
             checkDuplicationButton,
             setRandomNicknameButton,
@@ -142,40 +143,35 @@ final class NicknameViewController: BaseViewController {
     }
     
     private func setupConstraints() {
-        titleView.snp.makeConstraints { $0.top.equalTo(view.safeAreaLayoutGuide).offset(32)
+        titleView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(32)
             $0.leading.equalTo(view.safeAreaLayoutGuide).offset(20)
             $0.trailing.equalTo(view.safeAreaLayoutGuide).offset(-20)
         }
-        
         nicknameGuideLineLabel.snp.makeConstraints {
             $0.top.equalTo(titleView.snp.bottom).offset(40)
             $0.leading.equalTo(view.safeAreaLayoutGuide).offset(20)
         }
-        
-        nicknameLable.snp.makeConstraints {
+        nicknameLabel.snp.makeConstraints {
             $0.top.equalTo(nicknameGuideLineLabel.snp.bottom).offset(40)
             $0.leading.equalTo(view.safeAreaLayoutGuide).offset(20)
         }
-        
         nickNameTextField.snp.makeConstraints {
-            $0.top.equalTo(nicknameLable.snp.bottom).offset(12)
+            $0.top.equalTo(nicknameLabel.snp.bottom).offset(12)
             $0.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(20)
             $0.height.greaterThanOrEqualTo(52)
         }
-        
         checkDuplicationButton.snp.makeConstraints {
             $0.trailing.equalTo(nickNameTextField.snp.trailing).offset(-8)
             $0.width.equalTo(74)
             $0.height.equalTo(36)
             $0.centerY.equalTo(nickNameTextField.textField.snp.centerY)
         }
-        
         confirmButton.snp.makeConstraints {
             $0.bottom.equalTo(view.safeAreaLayoutGuide).offset(-20)
             $0.height.equalTo(56)
             $0.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(20)
         }
-        
         setRandomNicknameButton.snp.makeConstraints {
             $0.centerX.equalTo(view.snp.centerX)
             $0.bottom.equalTo(confirmButton.snp.top).offset(-20)
@@ -188,14 +184,13 @@ final class NicknameViewController: BaseViewController {
         let dashedBorder = CAShapeLayer()
         dashedBorder.strokeColor = UIColor.et_lineGray40.cgColor
         dashedBorder.fillColor = nil
-        dashedBorder.lineDashPattern = [4, 2] // 점선 (4pt 그려지고 2pt 비워짐)
+        dashedBorder.lineDashPattern = [4, 2]
         dashedBorder.lineWidth = 1
         dashedBorder.frame = button.bounds
         dashedBorder.path = UIBezierPath(
             roundedRect: button.bounds,
             cornerRadius: button.layer.cornerRadius
         ).cgPath
-        
         button.layer.addSublayer(dashedBorder)
     }
 }
@@ -206,43 +201,43 @@ extension NicknameViewController: View {
         bindOutput(reactor: reactor)
     }
     
-    func bindInput(reactor: NicknameReactor) {
-        setRandomNicknameButton.rx.tap
-            .map { Reactor.Action.randomButtonTapped }
-            .bind(to: reactor.action)
+    private func bindInput(reactor: NicknameReactor) {
+        nickNameTextField.textField.rx.controlEvent(.editingChanged)
+            .map {
+                EveryTipTextFieldStatus.editing
+            }
+            .bind(to: nickNameTextField.status)
             .disposed(by: disposeBag)
         
-        checkDuplicationButton.rx.tap
-            .withLatestFrom(nickNameTextField.textField.rx.text.orEmpty)
-            .map { Reactor.Action.checkDuplication(nickName: $0) }
-            .bind(to: reactor.action)
-            .disposed(by: disposeBag)
+        // 텍스트 변경 → Reactor
         
         nickNameTextField.action
-            .map { action in
-                switch action {
-                case .editingDidBegin:
-                    return Reactor.Action.textEditingDidBegin
-                case .editingDidEnd:
-                    return Reactor.Action.textEditingDidEnd
-                case .editingDidEndOnExit:
-                    return Reactor.Action.textDidEndOnExit
-                case .textChanged(text: let text):
-                    return Reactor.Action.textChanged(text)
-                }
+            .compactMap { action -> String? in
+                if case let .textChanged(text) = action { return text }
+                return nil
             }
+            .map { NicknameReactor.Action.textChanged($0) }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
+        
+        checkDuplicationButton.rx.tap
+            .map { NicknameReactor.Action.checkDuplication }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
+        setRandomNicknameButton.rx.tap
+            .map { NicknameReactor.Action.randomButtonTapped }
             .bind(to: reactor.action)
             .disposed(by: disposeBag)
         
         confirmButton.rx.tap
-            .map {
-                Reactor.Action.confirmButtonTapped
-            }
+            .map { NicknameReactor.Action.confirmButtonTapped }
             .bind(to: reactor.action)
             .disposed(by: disposeBag)
     }
     
-    func bindOutput(reactor: NicknameReactor) {
+    private func bindOutput(reactor: NicknameReactor) {
         reactor.state
             .map { $0.nicknameText }
             .distinctUntilChanged()
@@ -250,45 +245,38 @@ extension NicknameViewController: View {
             .disposed(by: disposeBag)
         
         reactor.state
-            .map { $0.isCheckedDuplication }
-            .distinctUntilChanged()
-            .subscribe(onNext: { [weak self] isChecked in
-                self?.confirmButton.backgroundColor = isChecked ? .et_brandColor2 : .et_textColor5
-            })
-        
+            .map { $0.validationResult.message }
+            .bind(to: nickNameTextField.guideMessageLabel.rx.text)
             .disposed(by: disposeBag)
         
         reactor.state
-            .map { $0.nicknameInputState }
-            .bind(with: self) { owner, state in
-                switch state {
-                case .normal:
-                    owner.nickNameTextField.guideMessageLabel.text = nil
-                    owner.nickNameTextField.status.onNext(.normal)
-                case .editing:
-                    owner.nickNameTextField.guideMessageLabel.text = nil
-                    owner.nickNameTextField.status.onNext(.editing)
-                    
-                case .success(let reason):
-                    owner.nickNameTextField.guideMessageLabel.text = reason.message
-                    owner.nickNameTextField.status.onNext(.success)
-                    
-                case .error(let reason):
-                    owner.nickNameTextField.guideMessageLabel.text = reason.message
-                    owner.nickNameTextField.status.onNext(.error)
-                    
-                case .notEnabled:
-                    owner.nickNameTextField.guideMessageLabel.text = nil
-                    owner.nickNameTextField.status.onNext(.notEnabled)
+            .map { $0.validationResult }
+            .distinctUntilChanged()
+            .map { $0 == .valid ? UIColor.et_brandColor2 : UIColor.et_textColor5 }
+            .bind(to: confirmButton.rx.backgroundColor)
+            .disposed(by: disposeBag)
+        
+        reactor.state
+            .map { $0.validationResult }
+            .distinctUntilChanged()
+            .map { validation -> EveryTipTextFieldStatus in
+                switch validation {
+                case .none:
+                    return .normal
+                case .valid:
+                    return .success
+                case .empty, .invalidFormat, .duplicated:
+                    return .error
                 }
             }
+            .bind(to: nickNameTextField.status)
             .disposed(by: disposeBag)
         
         reactor.pulse(\.$toastMessage)
             .compactMap { $0 }
-            .subscribe { [weak self] message in
+            .subscribe(onNext: { [weak self] message in
                 self?.showToast(message: message)
-            }
+            })
             .disposed(by: disposeBag)
     }
 }
--- EveryTip/Targets/EveryTipPresentation/Sources/Nickname/NicknameReactor.swift
+++ EveryTip/Targets/EveryTipPresentation/Sources/Nickname/NicknameReactor.swift
@@ -14,151 +14,111 @@ import ReactorKit
 import RxSwift
 
 final class NicknameReactor: Reactor {
-        
-    enum NicknameInputState {
-        case normal
-        case editing
-        case success(reason: NicknameValidationResult)
-        case error(reason: NicknameValidationResult)
-        case notEnabled
-    }
-    
-    enum NicknameValidationResult {
+    enum ValidationResult {
+        case none
+        case valid
         case empty
         case invalidFormat
         case duplicated
-        case success
 
-        var message: String {
+        var message: String? {
             switch self {
+            case .none:
+                return nil
+            case .valid:
+                return "사용 가능한 닉네임입니다."
             case .empty:
                 return "닉네임을 입력해주세요."
             case .invalidFormat:
                 return "닉네임 형식이 옳바르지 않습니다."
             case .duplicated:
                 return "이미 사용 중인 닉네임입니다."
-            case .success:
-                return "사용 가능한 닉네임입니다."
             }
         }
     }
 
-    // MARK: Reactor
-    
     enum Action {
-        case checkDuplication(nickName: String)
+        case textChanged(String)
+        case checkDuplication
         case randomButtonTapped
         case confirmButtonTapped
-        
-        case textEditingDidBegin
-        case textEditingDidEnd
-        case textDidEndOnExit
-        case textChanged(String)
     }
-    
+
     enum Mutation {
-        case setNickName(String)
-        case setIsCheckedDuplication(Bool)
-        case setNicknameInputState(NicknameInputState)
+        case setNickname(String)
+        case setValidation(ValidationResult)
         case setToast(String)
     }
 
     struct State {
-        var nicknameText: String?
-        var isCheckedDuplication: Bool = false
-        var nicknameInputState: NicknameInputState = .normal
+        var nicknameText: String = ""
+        var validationResult: ValidationResult = .none
         @Pulse var toastMessage: String?
     }
-    
-    var initialState: State = State()
-    
+
+    var initialState = State()
     private let userUseCase: UserUseCase
-    
+
     init(userUseCase: UserUseCase) {
         self.userUseCase = userUseCase
     }
     
     func mutate(action: Action) -> Observable<Mutation> {
         switch action {
+        case .textChanged(let text):
+            if text.isEmpty {
+                return .just(.setValidation(.empty))
+            }
+        
+            return Observable.concat(
+                .just(.setNickname(text)),
+                .just(.setValidation(.none))
+            )
+        
         case .checkDuplication:
-            // TODO: 현재 닉네임 중복 체크 API없어서 공백으로 기준삼아놨음
-            if currentState.nicknameText == nil {
-                return Observable.just(.setNicknameInputState(.error(reason: .duplicated)))
-            } else {
-                return Observable.concat(
-                    .just(.setIsCheckedDuplication(true)),
-                    .just(.setNicknameInputState(.success(reason: .success)))
-                )
+            let currentText = currentState.nicknameText
+            if currentText.isEmpty {
+                return .just(.setValidation(.empty))
+            } else if !currentText.checkRegex(type: .nickname) {
+                return .just(.setValidation(.invalidFormat))
             }
             
+            // TODO: 중복검사 useCase 삽입
+            return Observable.just(false)
+                .asObservable()
+                .flatMap { isDuplicated in
+                    let result: ValidationResult = isDuplicated ? .duplicated : .valid
+                    return Observable.just(Mutation.setValidation(result))
+                }
+
         case .randomButtonTapped:
             return userUseCase.fetchRamdomNickName()
                 .asObservable()
-                .flatMap { nickname in
-                    return Observable.concat(
-                        .just(.setNickName(nickname)),
-                        .just(.setIsCheckedDuplication(false))
-                    )
+                .flatMap { randomNick in
+                    Observable.concat([
+                        .just(.setNickname(randomNick)),
+                        .just(.setValidation(.none))
+                    ])
                 }
 
-        case .textChanged(let text):
-            let result: NicknameValidationResult? = {
-                if text.isEmpty {
-                    return .empty
-                }
-                
-                if !text.checkRegex(type: .nickname) {
-                    return .invalidFormat
-                }
-                
-                return nil
-            }()
-            
-            let fieldState: NicknameInputState = {
-                if let result = result {
-                    return .error(reason: result)
-                } else {
-                    return .editing
-                }
-            }()
-            
-            return .concat(
-                .just(.setIsCheckedDuplication(false)),
-                .just(.setNickName(text)),
-                .just(.setNicknameInputState(fieldState))
-            )
-        case .textEditingDidBegin:
-            return Observable.just(.setNicknameInputState(.editing))
-        case .textEditingDidEnd:
-            return Observable.just(.setNicknameInputState(.normal))
-        case .textDidEndOnExit:
-            return Observable.just(.setNicknameInputState(.normal))
-            
         case .confirmButtonTapped:
-            if currentState.isCheckedDuplication == false {
-                return Observable.just(.setToast("닉네임 중복 확인을 진행해주세요"))
+            if currentState.validationResult != .valid {
+                return .just(.setToast("닉네임 중복 확인을 해주세요."))
             }
-            
-            return Observable.concat(
-                Observable.just(.setToast("닉네임 설정이 완료되었어요"))
-            )
+            return .just(.setToast("닉네임 설정이 완료되었어요"))
         }
     }
-    
+
     func reduce(state: State, mutation: Mutation) -> State {
         var newState = state
-        
         switch mutation {
-        case .setNickName(let nickname):
-            newState.nicknameText = nickname
-        case .setIsCheckedDuplication(let isChecked):
-            newState.isCheckedDuplication = isChecked
-        case .setNicknameInputState(let state):
-            newState.nicknameInputState = state
-        case .setToast(let toast):
-            newState.toastMessage = toast
+        case .setNickname(let updated):
+            newState.nicknameText = updated
+        case .setValidation(let validation):
+            newState.validationResult = validation
+        case .setToast(let message):
+            newState.toastMessage = message
         }
-        
         return newState
     }
 }
