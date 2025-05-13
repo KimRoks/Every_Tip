dd232804d88520a1c819c0d75e17d02a8ff0b30c
KimRoks <kimroks3161@gmail.com>
2025-05-13 18:03:42 +0900

    refactor: MyInfo Reactor 기능 추가 및 개선
    - View 개선 코드 포함

--- EveryTip/Targets/EveryTipPresentation/Sources/MyInfo/MyInfoReactor.swift
+++ EveryTip/Targets/EveryTipPresentation/Sources/MyInfo/MyInfoReactor.swift
@@ -10,42 +10,69 @@ import Foundation
 
 import EveryTipDomain
 
+import EveryTipCore
+
 import ReactorKit
 import RxSwift
 
 class MyInfoReactor: Reactor {
+    private let tokenManager = TokenKeyChainManager.shared
+    
     enum Action {
         // TODO: 프로필 편집, 각 아이템 터치 처리
         case viewDidLoad
+        case viewWillAppear
+        
+        case agreementCellTapped
+        case logoutCellTapped
+        case logoutConfirmTapped
     }
     
     enum Mutation {
-        //viewDidLoad 시
         case setMyProfileData(MyProfile)
-        case setError(Error)
+        case setToast(String)
+        
+        // Set Navigation Signal
+        case setLogoutCellSignal
+        case setAgreementCellSignal
+        case setLogoutConfirmSignal(Bool)
     }
     
     struct State {
-        var myProfile: MyProfile?
-        var userName: String?
-        var subscribersCount: String?
-        var postedTipCount: String?
-        var savedTipCount: String?
+        var myProfile: MyProfile
         
-        var fetchError: Error?
+        @Pulse var toastMessage: String?
+        @Pulse var navigationSignal: NavigationSignal?
+        
+        enum NavigationSignal {
+            case agreement
+            case logout
+            case userContents
+        }
+        @Pulse var logoutConfirmSignal: Bool = false
     }
     
     let initialState: State
+
+    // API가 토큰 없을시 에러만 뱉기떄문에 더미데이터로 대체
+    private let guestProfile = MyProfile(
+        id: 0,
+        status: 0,
+        nickName: "게스트",
+        profileImageURL: nil,
+        email: "everytip",
+        registeredDate: "00",
+        tipCount: 0,
+        savedTipCount: 0,
+        subscriberCount: 0
+    )
     
     private let userUseCase: UserUseCase
     
     init(userUseCase: UserUseCase) {
         self.userUseCase = userUseCase
         self.initialState = State(
-            userName: "게스트",
-            subscribersCount: "0",
-            postedTipCount: "0",
-            savedTipCount: "0"
+            myProfile: guestProfile
         )
     }
     
@@ -55,9 +82,43 @@ class MyInfoReactor: Reactor {
             return userUseCase.fetchMyProfile()
                 .asObservable()
                 .map(Mutation.setMyProfileData)
-                .catch { error in
-                    Observable.just(Mutation.setError(error))
+                .catch { [weak self] _ in
+                    guard let self = self else {
+                        return .just(.setToast("잠시 후 다시 시도해주세요"))
+                    }
+                    return .just(.setMyProfileData(self.guestProfile))
                 }
+            
+        case .viewWillAppear:
+            return userUseCase.fetchMyProfile()
+                .asObservable()
+                .map(Mutation.setMyProfileData)
+                .catch { [weak self] _ in
+                    guard let self = self else {
+                        return .just(.setToast("잠시 후 다시 시도해주세요"))
+                    }
+                    return .just(.setMyProfileData(self.guestProfile))
+                }
+            
+        case .logoutCellTapped:
+            return .just(.setLogoutCellSignal)
+            
+        case .logoutConfirmTapped:
+            if tokenManager.isLoggedIn {
+                tokenManager.deleteToken(type: .access)
+                tokenManager.deleteToken(type: .refresh)
+                
+                return .concat(
+                    .just(.setMyProfileData(guestProfile)),
+                    .just(.setToast("로그아웃 되었어요")),
+                    .just(.setLogoutConfirmSignal(true))
+                )
+            } else {
+                return .just(.setToast("로그인이 되어있지않아요."))
+            }
+            
+        case .agreementCellTapped:
+            return .just(.setAgreementCellSignal)
         }
     }
     
@@ -67,12 +128,15 @@ class MyInfoReactor: Reactor {
         switch mutation {
         case .setMyProfileData(let myProfile):
             newState.myProfile = myProfile
-            newState.userName = myProfile.nickName
-            newState.subscribersCount = myProfile.subscriberCount.toAbbreviatedString()
-            newState.postedTipCount = myProfile.tipCount.toAbbreviatedString()
-            newState.savedTipCount = myProfile.savedTipCount.toAbbreviatedString()
-        case .setError(let error):
-            newState.fetchError = error
+        case .setToast(let message):
+            newState.toastMessage = message
+            
+        case .setLogoutConfirmSignal(let signal):
+            newState.logoutConfirmSignal = signal
+        case .setAgreementCellSignal:
+            newState.navigationSignal = .agreement
+        case .setLogoutCellSignal:
+            newState.navigationSignal = .logout
         }
         
         return newState
