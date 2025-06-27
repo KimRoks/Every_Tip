a72adf04ea5e5ad01dfad39df21367524959a9f5
KimRoks <kimroks3161@gmail.com>
2025-06-27 17:24:33 +0900

    feat: EditProfile VC, Reacotr,Coordinator 작성

---
+++ EveryTip/Targets/EveryTipPresentation/Sources/EditProfile/EditProfileCoordinator.swift
@@ -0,0 +1,49 @@
+//
+//  EditProfileCoordinator.swift
+//  EveryTipPresentation
+//
+//  Created by 김경록 on 6/26/25.
+//  Copyright © 2025 EveryTip. All rights reserved.
+//
+
+import UIKit
+
+import EveryTipDomain
+
+import Swinject
+
+protocol EditProfileCoordinator: Coordinator {
+    
+}
+
+final class DefaultEditProfileCoordinator: EditProfileCoordinator {
+    var parentCoordinator: (any Coordinator)?
+    
+    var childCoordinators: [any Coordinator] = []
+    
+    var navigationController: UINavigationController
+    
+    
+    init(navigationController: UINavigationController,
+         myNickName: String) {
+        self.navigationController = navigationController
+        self.myNickName = myNickName
+    }
+    
+    private let myNickName: String
+    
+    func start() {
+        guard let authUseCase = Container.shared.resolve(AuthUseCase.self) else { fatalError("의존성 주입이 옳바르지 않습니다!")
+        }
+        
+        let reactor = EditProfileReactor(authUseCase: authUseCase, nickName: myNickName)
+        
+        let vc = EditProfileViewController(reactor: reactor)
+        vc.coordinator = self
+        navigationController.pushViewController(vc, animated: true)
+    }
+    
+    func didFinish() {
+        parentCoordinator?.remove(child: self)
+    }
+}
---
+++ EveryTip/Targets/EveryTipPresentation/Sources/EditProfile/EditProfileViewController.swift
@@ -0,0 +1,189 @@
+//
+//  EditProfileViewController.swift
+//  EveryTipPresentation
+//
+//  Created by 김경록 on 6/26/25.
+//  Copyright © 2025 EveryTip. All rights reserved.
+//
+
+import UIKit
+
+import SnapKit
+
+import ReactorKit
+import RxSwift
+
+final class EditProfileViewController: BaseViewController {
+    
+    weak var coordinator: EditProfileCoordinator?
+    var disposeBag = DisposeBag()
+        
+    private let roundedBackgroundView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.setRoundedCorners(
+            radius: 15,
+            corners: .layerMinXMinYCorner, .layerMaxXMinYCorner
+        )
+        
+        return view
+    }()
+    
+    private let profileImageButton: UIButton = {
+        let button = UIButton()
+        button.setImage(.et_getImage(for: .blankImage), for: .normal)
+        button.layer.cornerRadius = 45
+        button.clipsToBounds = true
+        
+        return button
+    }()
+    
+    private lazy var nickNameLabel: UILabel = {
+        let label = UILabel()
+        
+        label.font = .et_pretendard(style: .semiBold, size: 23)
+        label.textColor = .et_textColorBlack70
+        
+        return label
+    }()
+    
+    private let optionTableView: UITableView = {
+        let tableView = UITableView()
+        
+        return tableView
+    }()
+    
+    init(reactor: EditProfileReactor) {
+        super.init(nibName: nil, bundle: nil)
+        self.reactor = reactor
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .et_brandColor2
+        setupLayout()
+        setupConstraints()
+        setupTableView()
+        profileImageButton.addTarget(self, action: #selector(dd), for: .touchUpInside)
+    }
+    
+    @objc
+    func dd() {
+        print("eqwreewqr")
+    }
+    
+    private func setupLayout() {
+        view.addSubViews(
+            roundedBackgroundView
+        )
+        
+        roundedBackgroundView.addSubViews(
+            profileImageButton,
+            nickNameLabel,
+            optionTableView
+        )
+    }
+    
+    private func setupConstraints() {
+        roundedBackgroundView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(10)
+            $0.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            $0.bottom.equalTo(view)
+        }
+        
+        profileImageButton.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(51)
+            $0.width.height.equalTo(90)
+            $0.centerX.equalToSuperview()
+        }
+        
+        nickNameLabel.snp.makeConstraints {
+            $0.top.equalTo(profileImageButton.snp.bottom).offset(18)
+            $0.centerX.equalToSuperview()
+        }
+        
+        optionTableView.snp.makeConstraints {
+            $0.top.equalTo(nickNameLabel.snp.bottom).offset(40)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+    
+    private func setupTableView() {
+        optionTableView.register(
+            MyInfoTableViewCell.self,
+            forCellReuseIdentifier: MyInfoTableViewCell.reuseIdentifier
+        )
+        optionTableView.isScrollEnabled = false
+    }
+}
+
+extension EditProfileViewController: View {
+    func bind(reactor: EditProfileReactor) {
+        bindInput(reactor: reactor)
+        bindOutput(reactor: reactor)
+    }
+    
+    func bindInput(reactor: EditProfileReactor) {
+        
+        optionTableView.rx.itemSelected
+            .map { Reactor.Action.itemSelected($0.row) }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
+        profileImageButton.rx.tap
+            .map{ Reactor.Action.imageButtonTapped }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+    }
+    
+    func bindOutput(reactor: EditProfileReactor) {
+        reactor.state
+            .map { $0.options }
+            .bind(to: optionTableView.rx.items(
+                cellIdentifier: MyInfoTableViewCell.reuseIdentifier,
+                cellType: MyInfoTableViewCell.self)
+            ) { row, item, cell in
+                switch item {
+                case .changeNickname:
+                    cell.leftLabel.text = "비밀번호 변경"
+                    cell.accessoryType = .disclosureIndicator
+                case .changePassword:
+                    cell.leftLabel.text = "회원 탈퇴"
+                    cell.leftLabel.textColor = .red
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        reactor.pulse(\.$changePasswordSignal)
+            .filter { $0 == true }
+            .bind { [weak self] _ in
+                print("비밀번호 변경으로 이동")
+            }
+            .disposed(by: disposeBag)
+        
+        reactor.pulse(\.$deleteAccountSignal)
+            .filter { $0 == true }
+            .bind { [weak self] _ in
+                //회원탈퇴 alert
+                print("회원탈퇴")
+            }
+            .disposed(by: disposeBag)
+        
+        reactor.pulse(\.$toastMessage)
+            .subscribe(onNext: { [weak self] message in
+                guard let message = message else { return }
+                print(message)
+                self?.showToast(message: message)
+            })
+            .disposed(by: disposeBag)
+        
+        reactor.state
+            .map { $0.nickName }
+            .bind(to: nickNameLabel.rx.text)
+            .disposed(by: disposeBag)
+    }
+}
