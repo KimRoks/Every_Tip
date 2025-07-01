fd7d92b6f31a87ab881edf8db6ac6488e40b6f23
KimRoks <kimroks3161@gmail.com>
2025-07-01 20:46:23 +0900

    refactor: PresignedUrlDTO 수정

--- EveryTip/Targets/EveryTipData/Sources/Services/DTO/Tip/PresignedURLDTO.swift
+++ EveryTip/Targets/EveryTipData/Sources/Services/DTO/Tip/PresignedURLDTO.swift
@@ -1,19 +1,19 @@
 //
-//  PresignedURLDTO.swift
+//  PresignedUrlDTO.swift
 //  EveryTipData
 //
-//  Created by 김경록 on 6/17/25.
+//  Created by 김경록 on 6/29/25.
 //  Copyright © 2025 EveryTip. All rights reserved.
 //
 
 import Foundation
 
-struct PresignedURLDTO: Codable {
+public struct PresignedUrlDTO: Codable {
     let code: String
     let message: String
-    let data: URLData?
-
-    struct URLData: Codable {
+    let data: Datum
+    
+    public struct Datum: Codable {
         let url: String
     }
 }
