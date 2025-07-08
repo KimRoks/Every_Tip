cf0c36b807aa20e13f5deef16aab2c10c1a48e8a
KimRoks <kimroks3161@gmail.com>
2025-07-08 10:53:10 +0900

    feat: 유저 컨텐츠 뷰에서 쓰일 공용 placeholderView

---
+++ EveryTip/Targets/EveryTipPresentation/Sources/Common/ViewComponents/UserContentPlaceholderView.swift
@@ -0,0 +1,81 @@
+//
+//  UserContentPlaceholderView.swift
+//  EveryTipPresentation
+//
+//  Created by 김경록 on 7/8/25.
+//  Copyright © 2025 EveryTip. All rights reserved.
+//
+
+import UIKit
+
+import SnapKit
+
+final class UserContentPlaceholderView: UIView {
+    
+    enum UserContentType {
+        case follower
+        case following
+        case writtenTip
+        case savedTip
+        
+        var message: String {
+            switch self {
+            case .follower:
+                return "구독자가 비어있어요. 팁을 작성해서 구독자를 늘려보세요!"
+            case .following:
+                return "구독 중인 유저가 없어요. 마음에 드는 유저를 구독해보세요!"
+            case .writtenTip:
+                return "작성한 팁이 없어요. 팁을 작성해보세요!"
+            case .savedTip:
+                return "저장한 팁이 없어요. 마음에 드는 팁을 저장해보세요!"
+            }
+        }
+    }
+    
+    private let blankImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = .et_getImage(for: .placeholder)
+        return imageView
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = .et_pretendard(style: .regular, size: 14)
+        label.textColor = .et_textColorBlack10
+        return label
+    }()
+    
+    init(type: UserContentType) {
+        super.init(frame: .zero)
+        setupLayout()
+        configure(with: type)
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupLayout() {
+        addSubview(blankImageView)
+        addSubview(descriptionLabel)
+    }
+    
+    private func setupConstraints() {
+        blankImageView.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.width.height.equalTo(86)
+            $0.top.equalToSuperview().offset(204)
+        }
+        
+        descriptionLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(blankImageView.snp.bottom).offset(8)
+        }
+    }
+    
+    
+    private func configure(with type: UserContentType) {
+        descriptionLabel.text = type.message
+    }
+}
